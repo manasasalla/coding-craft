155d633b486dc46ece5c236d9934e7d5fa698b0e
manasasalla <manasasalla146@gmail.com>
2025-02-08 07:16:41 +0530

    removing credentials

--- app.py
+++ app.py
@@ -1,36 +1,10 @@
-from flask import Flask, render_template, request
-from pydrive2.auth import GoogleAuth
-from pydrive2.drive import GoogleDrive
+from flask import Flask, render_template
 
-app = Flask(__name__)
-
-gauth = GoogleAuth()
-
-#gauth.LocalWebserverAuth()
-drive = GoogleDrive(gauth)
-
-@app.route("/")
+app=Flask(__name__)
+@app.route('/')
 def index():
-    return render_template("index.html")
-
-@app.route("/upload", methods=["POST"])
-def upload_file():
-    if "file" not in request.files:
-        return "No file part"
-
-    file = request.files["file"]
-    if file.filename == "":
-        return "No selected file"
-
-    gfile = drive.CreateFile({'title': file.filename})
-    gfile.SetContentString(file.read().decode("latin-1"))  # Adjust encoding if needed
-    gfile.Upload()
-
-    return "File uploaded successfully to Google Drive!"
-
+    return render_template('index.html')
 
 if __name__ == "__main__":
     #app.run(debug=False)
       app.run( debug=True)
-
-
